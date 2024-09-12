387b26cedb3c98472e462f2559596946f2291d12
leo silva <leo@pop-os.localdomain>
2024-09-11 22:10:22 -0400

    ajustada largura do menu

--- src/components/Tray.tsx
+++ src/components/Tray.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const Tray: FC<Props> = ({ targetRef: ref, description, camNumber }) => {
    return (
-      <div className='absolute left-1/2 transform -translate-x-1/2 bottom-1 flex justify-between items-center bg-[#00000030] rounded-lg invisible group-hover:visible transition-all duration-1000 ease-in-out py-2 px-4 space-x-3'>
+      <div className='absolute left-1/2 transform -translate-x-1/2 bottom-1 flex justify-between items-center bg-[#00000030] rounded-lg invisible group-hover:visible transition-all duration-1000 ease-in-out py-2 px-4 space-x-3 min-w-48'>
          <BackBtn camNumber={camNumber} />
          <span className='text-gray-200'>{description}</span>
          <SnapshotBtn targetRef={ref} description={description} />
