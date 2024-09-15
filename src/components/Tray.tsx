424d543e5c0b74d9f6ac39cf7b539480bb6ead9e
leo silva <leo@pop-os.localdomain>
2024-09-15 19:34:02 -0400

    alterado tamnho legendas 2

--- src/components/Tray.tsx
+++ src/components/Tray.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const Tray: FC<Props> = ({ targetRef: ref, description }) => {
    return (
-      <div className='absolute left-1/2 transform -translate-x-1/2 bottom-1 flex justify-between items-center bg-[#00000030] rounded-lg invisible group-hover:visible transition-all duration-1000 ease-in-out py-2 px-4 space-x-3 min-w-48'>
+      <div className='absolute left-1/2 transform -translate-x-1/2 bottom-1 flex justify-between items-center bg-[#00000030] rounded-lg invisible group-hover:visible transition-all duration-1000 ease-in-out py-2 px-4 space-x-3 min-w-[300px]'>
          <BackBtn />
          <span className='text-gray-200 text-3xl'>{description}</span>
          <SnapshotBtn targetRef={ref} description={description} />
