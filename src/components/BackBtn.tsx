937523afa69388abf31998b975e187adbd006790
leo silva <leo@pop-os.localdomain>
2024-09-15 19:26:56 -0400

    alterado tamnho icones 2

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -3,9 +3,9 @@ import { state } from '../store';
 
 export const BackBtn: FC = () => {
    return (
-      <button className={`bg-[#FFFFFF30] px-1 py-1 rounded-full inline-flex items-center w-16 invisible group-hover:visible transition-all duration-1000 ease-in-out`}
+      <button className={`bg-[#FFFFFF30] px-1 py-1 rounded-full inline-flex items-center w-12 invisible group-hover:visible transition-all duration-1000 ease-in-out`}
          onClick={() => state.viewState = { type: 'Multi' }}>
-         <svg className="w-14 h-14 text-gray-800">
+         <svg className="w-10 h-10 text-gray-800">
             <use href='#back' />
          </svg>
       </button>
--- src/components/Tray.tsx
+++ src/components/Tray.tsx
@@ -11,7 +11,7 @@ export const Tray: FC<Props> = ({ targetRef: ref, description }) => {
    return (
       <div className='absolute left-1/2 transform -translate-x-1/2 bottom-1 flex justify-between items-center bg-[#00000030] rounded-lg invisible group-hover:visible transition-all duration-1000 ease-in-out py-2 px-4 space-x-3 min-w-48'>
          <BackBtn />
-         <span className='text-gray-200 text-lg'>{description}</span>
+         <span className='text-gray-200 text-3xl'>{description}</span>
          <SnapshotBtn targetRef={ref} description={description} />
       </div>
    );
